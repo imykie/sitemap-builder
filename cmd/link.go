338d1227b372731211a7bd7b35e5aad4b7b2b1d2
Michael Olatunji <tundexmike@gmail.com>
2021-06-29 00:06:30 +0100

    read, parse and print html

--- cmd/link.go
+++ cmd/link.go
@@ -1,6 +1,10 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"golang.org/x/net/html"
+)
 
 type Link struct {
 	Href string
@@ -8,5 +12,21 @@ type Link struct {
 }
 
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "  ")
 	return nil, nil
+}
+
+func dfs(n *html.Node, padding string) {
+	msg := n.Data
+	if n.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+ "  ")
+	}
 }
