10c02031821a95ed069228dd331a45d654effbe4
Michael Olatunji <tundexmike@gmail.com>
2021-07-04 01:47:28 +0100

    clean hrefs

--- main.go
+++ main.go
@@ -3,92 +3,46 @@ package main
 import (
 	"flag"
 	"fmt"
-	link "sitemap-builder/cmd"
 	"net/http"
+	"net/url"
+	link "sitemap-builder/cmd"
+	"strings"
 )
 
-const exampleHTML = `
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html class="ie ie6 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="ie ie7 lt-ie9 lt-ie8"        lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="ie ie8 lt-ie9"               lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="ie ie9"                      lang="en"> <![endif]-->
-<!--[if !IE]><!-->
-<html lang="en" class="no-ie">
-<!--<![endif]-->
-
-<head>
-  <title>Gophercises - Coding exercises for budding gophers</title>
-</head>
-
-<body>
-  <section class="header-section">
-    <div class="jumbo-content">
-      <div class="pull-right login-section">
-        Already have an account?
-        <a href="#" class="btn btn-login">Login <i class="fa fa-sign-in" aria-hidden="true"></i></a>
-      </div>
-      <center>
-        <img src="https://gophercises.com/img/gophercises_logo.png" style="max-width: 85%; z-index: 3;">
-        <h1>coding exercises for budding gophers</h1>
-        <br/>
-        <form action="/do-stuff" method="post">
-          <div class="input-group">
-            <input type="email" id="drip-email" name="fields[email]" class="btn-input" placeholder="Email Address" required>
-            <button class="btn btn-success btn-lg" type="submit">Sign me up!</button>
-            <a href="/lost">Lost? Need help?</a>
-          </div>
-        </form>
-        <p class="disclaimer disclaimer-box">Gophercises is 100% FREE, but is currently in beta. There will be bugs, and things will be changing significantly over the coming weeks.</p>
-      </center>
-    </div>
-  </section>
-  <section class="footer-section">
-    <div class="row">
-      <div class="col-md-6 col-md-offset-1 vcenter">
-        <div class="quote">
-          "Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do." - Pele
-        </div>
-      </div>
-      <div class="col-md-4 col-md-offset-0 vcenter">
-        <center>
-          <img src="https://gophercises.com/img/gophercises_lifting.gif" style="width: 80%">
-          <br/>
-          <br/>
-        </center>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-10 col-md-offset-1">
-        <center>
-          <p class="disclaimer">
-            Michael Olatunji (<a href="https://twitter.com/_imyke">@_imyke</a>)
-          </p>
-        </center>
-      </div>
-    </div>
-  </section>
-</body>
-</html>`
-
 func main() {
-	url := flag.String("url", "https://github.com", "The URL you want to build sitemap for")
+	urlFlag := flag.String("url", "https://github.com", "The URL you want to build sitemap for")
 	flag.Parse()
-	//r := strings.NewReader(exampleHTML)
 
-	resp, err := http.Get(*url)
+	resp, err := http.Get(*urlFlag)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	//io.Copy(os.Stdout, resp.Body)
-	//fmt.Println(*url, resp)
+
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+
+	base := baseUrl.String()
+
 	links, err := link.Parse(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, link := range links {
-		fmt.Printf("%+v\n", link)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+
+	for _, href := range hrefs {
+		fmt.Println(href)
 	}
 }
